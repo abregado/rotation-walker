a79c7a11de945e853b2b155fe284617a08ec41ac
Ben Buckton <contact@benbuckton.com>
2021-08-28 22:09:24 +0200

    New indicators

---
+++ Assets/Scripts/Indicators/LightIndicator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class LightIndicator : MonoBehaviour, IIndicate {
+    private Light[] _lights;
+
+    private bool _state;
+
+    
+    public void Init() {
+        _lights = GetComponentsInChildren<Light>();
+        
+        StopEffects();
+    }
+
+    private void StartEffects() {
+        foreach (var light in _lights) {
+            light.enabled = true;
+        }
+    }
+
+    private void StopEffects() {
+        foreach (var light in _lights) {
+            light.enabled = false;
+        }
+    }
+
+
+    public void SetState(int state) {
+        _state = state > 0;
+        if (_state) {
+            StartEffects();
+        }
+        else {
+            StopEffects();
+        }
+    }
+
+    public void SetState(bool state) {
+        _state = state;
+        if (_state) {
+            StartEffects();
+        }
+        else {
+            StopEffects();
+        }
+    }
+}
---
+++ Assets/Scripts/Indicators/ParticleIndicator.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+public class ParticleIndicator : MonoBehaviour, IIndicate {
+    private ParticleSystem[] _particleSystems;
+    private VisualEffect[] _visualEffects;
+
+    private bool _state;
+
+    
+    public void Init() {
+        _particleSystems = GetComponentsInChildren<ParticleSystem>();
+        _visualEffects = GetComponentsInChildren<VisualEffect>();
+
+        SetState(false);
+    }
+
+    private void StartEffects() {
+        foreach (var system in _particleSystems) {
+            system.Play();
+        }
+
+        foreach (var effect in _visualEffects) {
+            effect.Play();
+        }
+    }
+
+    private void StopEffects() {
+        
+        foreach (ParticleSystem system in _particleSystems) {
+            system.Stop();
+            system.Clear();
+        }
+        
+        foreach (VisualEffect effect in _visualEffects) {
+            effect.Stop();
+        }
+    }
+
+
+    public void SetState(int state) {
+        _state = state > 0;
+        if (_state) {
+            StartEffects();
+        }
+        else {
+            StopEffects();
+        }
+    }
+
+    public void SetState(bool state) {
+        _state = state;
+        if (_state) {
+            StartEffects();
+        }
+        else {
+            StopEffects();
+        }
+    }
+}
