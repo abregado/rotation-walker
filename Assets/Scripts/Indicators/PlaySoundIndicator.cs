3481324f20f97b5a41b1b5de76bb53a969a8f2de
Tom Mason <wheybags@wheybags.com>
2021-08-29 18:53:01 +0100

    better lever sound

--- Assets/Scripts/Indicators/PlaySoundIndicator.cs
+++ Assets/Scripts/Indicators/PlaySoundIndicator.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaySoundIndicator : MonoBehaviour, IIndicate {
-
-    private bool _state;
-
+public class PlaySoundIndicator : MonoBehaviour, IIndicate
+{
+    public bool playOnEnable = true;
+    public bool playOnDisable = false;
+    private bool _state = false;
+    
     public void Init() {
-        SetState(false);
+        //SetState(false);
     }
 
     public void SetState(bool state) {
+        if (state == _state)
+            return;
+
+        _state = state;
+        
         foreach (var source in this.gameObject.GetComponents<AudioSource>())
         {
-            if (state)
+            if ((state && playOnEnable) || (!state && playOnDisable))
                 source.Play();
             else
                 source.Stop();
