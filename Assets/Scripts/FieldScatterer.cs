3bc55e2838ba1df8636c847352639ea8d374deef
Ben Buckton <contact@benbuckton.com>
2021-08-29 19:15:47 +0200

    New terrain randomziers

---
+++ Assets/Scripts/FieldScatterer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FieldScatterer: MonoBehaviour {
+    public float radius = 3;
+
+    public void Start() {
+        foreach (Transform child in transform) {
+            child.localPosition = new Vector3(Random.Range(radius * -1, radius), 0, Random.Range(radius * -1, radius));
+        }
+    }
+    
+}
---
+++ Assets/Scripts/TransformRandomzier.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TransformRandomzier: MonoBehaviour {
+
+    public Vector3 scaleMin;
+    public Vector3 scaleMax;
+
+    public Vector3 rotation;
+    public void Start() {
+        float rotRand_x = Random.value;
+        float rotRand_y = Random.value;
+        float rotRand_z = Random.value;
+        transform.rotation = Quaternion.Euler(new Vector3(rotation.x*rotRand_x,rotation.y*rotRand_y,rotation.z*rotRand_z));
+
+        float scale_x = Random.Range(scaleMin.x, scaleMax.x);
+        float scale_y = Random.Range(scaleMin.y, scaleMax.y);
+        float scale_z = Random.Range(scaleMin.z, scaleMax.z);
+
+        transform.localScale = new Vector3(scale_x, scale_y, scale_z);
+    }
+}
